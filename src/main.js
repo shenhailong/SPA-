142e393f34ff20450c5510789daa0fb19d75276a
shenhailong <shenhailong@rocoinfo.com>
2018-06-12 13:13:09 +0800

    spa:

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import App from './App'
 import router from './router'
 import axios  from 'axios'
 import store from './store'
-
+import VueQuillEditor from 'vue-quill-editor'
+Vue.use(VueQuillEditor)
 //Vue.use(axios)
 
 Vue.config.productionTip = false
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,15 +3,11 @@
     <header>头部</header>
     <div class="content">
       <!--banner-->
-      <div ref="bannerSwiper"
-           class="swiper-container swiper-container1" style="height:146px;">
+      <div ref="bannerSwiper" class="swiper-container swiper-container1" style="height:146px;">
         <div class="swiper-wrapper" style="height:146px;">
-          <div v-for="banner in banners"
-            :key="banner.id"
-            class="swiper-slide">
+          <div v-for="banner in banners" :key="banner.id" class="swiper-slide">
             <a :href="banner.link">
-              <img v-bind:data-src="banner.loadableImgUrl" class="swiper-lazy"
-                 alt="">
+              <img v-bind:data-src="banner.loadableImgUrl" class="swiper-lazy" alt="">
             </a>
             <div class="swiper-pagination"></div>
           </div>
@@ -19,6 +15,8 @@
       </div>
       <!--banner-->
     </div>
+    <quill-editor v-model="content" @change="onEditorChange($event)" ref="myQuillEditor" :options="editorOption" @blur="onEditorBlur($event)" @focus="onEditorFocus($event)" @ready="onEditorReady($event)">
+    </quill-editor>
     <nav-bar></nav-bar>
   </div>
 </template>
@@ -30,52 +28,69 @@ import navBar from "../components/NavBar"
 //import Swiper from 'Swiper'
 export default {
   name: 'app',
-  components:{
-      navBar
+  components: {
+    navBar
   },
-  data(){
+  data() {
     return {
-      banners :[],
-      loaded : false
+      banners: [],
+      loaded: false,
+      content: '<h2>I am Example</h2>',
+      editorOption: {
+        // some quill options
+      }
     }
   },
-  created(){
+  created() {
     this.fetchData();
   },
-  mounted(){
+  mounted() {
 
   },
-  methods:{
+  methods: {
     //轮播图
-    fetchData: function(){
-      var self=this;
-      axios.get("/static/data/index.json").then((res)=>{
-        if(res.data.code==='1'){
-          self.loaded  = false
+    fetchData: function () {
+      var self = this;
+      axios.get("/static/data/index.json").then((res) => {
+        if (res.data.code === '1') {
+          self.loaded = false
           self.banners = res.data.data.WAP_HOME_SLIDER_BANNER;
-          self.loaded  =true;
+          self.loaded = true;
         }
       })
+    },
+    onEditorBlur(editor) {
+      console.log('editor blur!', editor)
+    },
+    onEditorFocus(editor) {
+      console.log('editor focus!', editor)
+    },
+    onEditorReady(editor) {
+      console.log('editor ready!', editor)
+    },
+    onEditorChange({ editor, html, text }) {
+      console.log('editor change!', editor, html, text)
+      this.content = html
     }
 
   },
-  watch:{
-    "loaded":function(val){
-      this.$nextTick(function(){
-        try{
-            this.bannerSwiper && this.bannerSwiper.destroy()
-            this.bannerSwiper = new Swiper(this.$refs.bannerSwiper, {
-              autoplay: 5000,
-              speed: 800,
-              loop: true,
-              lazyLoading: true,
-              lazyLoadingOnTransitionStart: true,
-              lazyLoadingInPrevNext: true,
-              lazyLoadingInPrevNextAmount: 2
-            })
-          }catch(e){
-            console.log(e)
-          }
+  watch: {
+    "loaded": function (val) {
+      this.$nextTick(function () {
+        try {
+          this.bannerSwiper && this.bannerSwiper.destroy()
+          this.bannerSwiper = new Swiper(this.$refs.bannerSwiper, {
+            autoplay: 5000,
+            speed: 800,
+            loop: true,
+            lazyLoading: true,
+            lazyLoadingOnTransitionStart: true,
+            lazyLoadingInPrevNext: true,
+            lazyLoadingInPrevNextAmount: 2
+          })
+        } catch (e) {
+          console.log(e)
+        }
       })
     }
   }
